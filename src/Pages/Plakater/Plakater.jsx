92a53986a0eb1ea899b0b6513891e8e0dbf1ac42
Rasmus Jensen <rasmusdj96@gmail.com>
2023-02-03 10:54:10 +0100

    Added genrelist options

--- src/Pages/Plakater/Plakater.jsx
+++ src/Pages/Plakater/Plakater.jsx
@@ -1,20 +1,38 @@
 import { Layout } from "../../Components/App/Layout/Layout";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Plakater = () => {
 	return (
-		<Layout title="Plakater" description="Se vores udvalg af plakater">
-			Nostrud ut ad magna veniam pariatur laborum nisi eiusmod tempor velit
-			irure minim ex veniam. Ad cupidatat ut ex dolor ex incididunt dolor
-			commodo cupidatat velit dolore aliquip. Nostrud anim aliqua nisi magna.
-			Laboris aliquip dolore irure labore mollit adipisicing esse. Culpa est
-			dolor commodo pariatur. Eiusmod excepteur cupidatat consequat sint
-			proident. Sunt sunt anim voluptate mollit ea consectetur duis magna ex.
-			Commodo laboris qui enim aute. In officia minim laboris velit officia anim
-			consequat. Laborum reprehenderit magna eu adipisicing ex elit ex anim.
-			Proident deserunt irure sit consequat nulla incididunt nisi magna sunt
-			anim elit. Dolor id minim et esse veniam minim id id. Sunt laboris nulla
-			nisi amet elit consectetur aliqua nisi ex laboris ut.
+		<Layout title="Plakater">
+			<GenreList />
 		</Layout>
+	);
+};
+
+const GenreList = () => {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		const getData = async () => {
+			const result = await axios.get("http://localhost:4000/genre");
+			setData(result.data);
+		};
+		getData();
+	}, [setData]);
+
+	return (
+		<ul>
+			{data &&
+				data.map((genre) => {
+					return (
+						<li key={genre.id}>
+							<Link to={`/posters/${genre.slug}`}>{genre.title}</Link>
+						</li>
+					);
+				})}
+		</ul>
 	);
 };
 
