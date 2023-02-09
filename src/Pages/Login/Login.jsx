394e55b675ba432fa124c4fe94416ecf1f0e410c
Rasmus Jensen <rasmusdj96@gmail.com>
2023-02-09 09:24:51 +0100

    added login token

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,5 +1,48 @@
+import axios from "axios";
 import { Layout } from "../../Components/App/Layout/Layout";
 
 export const Login = () => {
-	return <Layout title="Login"></Layout>;
+	const submitHandle = async (e) => {
+		// console.log(e.target.form.username.value);
+
+		const formdata = new URLSearchParams();
+		formdata.append("username", e.target.form.username.value);
+		formdata.append("password", e.target.form.password.value);
+		// console.log(...formdata);
+
+		const endpoint = `http://localhost:4000/login`;
+		try {
+			const result = await axios.post(endpoint, formdata);
+			console.log(result.data.access_token);
+			handleSessionData(result.data.access_token);
+		} catch (err) {
+			console.error(`Kunne ikke logge ind: ${err}`);
+		}
+	};
+
+	const handleSessionData = (data) => {
+		if (data) {
+			sessionStorage.setItem("token", JSON.stringify(data));
+		}
+	};
+
+	return (
+		<Layout title="Login">
+			<form method="POST">
+				<div>
+					<label htmlFor="username">Brugernavn</label>
+					<input type="text" name="username" id="password" />
+				</div>
+				<div>
+					<label htmlFor="password">Adgangskode:</label>
+					<input type="password" name="password" id="password" />
+				</div>
+				<div>
+					<button type="button" onClick={(e) => submitHandle(e)}>
+						Login
+					</button>
+				</div>
+			</form>
+		</Layout>
+	);
 };
