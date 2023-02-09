d7e8e797b059337263e3b95f5916988224006ba7
Rasmus Jensen <rasmusdj96@gmail.com>
2023-02-09 10:33:57 +0100

    Added provider

--- src/index.js
+++ src/index.js
@@ -5,18 +5,21 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "./Components/Styled/Theme";
 import { GlobalStyle } from "./Components/Styled/Global.style";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./Components/Providers/AuthProvider";
 // import { PosterProvider } from "./Components/App/PosterProvider/PosterProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		{/* <PosterProvider> */}
-		<ThemeProvider theme={theme}>
-			<GlobalStyle />
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</ThemeProvider>
+		<AuthProvider>
+			<ThemeProvider theme={theme}>
+				<GlobalStyle />
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</ThemeProvider>
+		</AuthProvider>
 		{/* </PosterProvider> */}
 	</React.StrictMode>
 );
