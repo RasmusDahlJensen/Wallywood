37e29500db3e2d46c044ca0a6f2a84b6cfb60dcb
Rasmus Jensen <rasmusdj96@gmail.com>
2023-02-14 09:26:28 +0100

    Added cart

--- src/Components/App/AppRouter/AppRouter.jsx
+++ src/Components/App/AppRouter/AppRouter.jsx
@@ -9,7 +9,7 @@ import { Notfound } from "../../../Pages/NotFound/Notfound";
 import { About } from "../../../Pages/About/About";
 import { Kontakt } from "../../../Pages/Kontakt/Kontakt";
 import { Login } from "../../../Pages/Login/Login";
-// import { Cart } from "../../../Pages/Cart/Cart";
+import { Cart } from "../../../Pages/Cart/Cart";
 
 const AppRouter = () => {
 	return (
@@ -21,7 +21,7 @@ const AppRouter = () => {
 			</Route>
 			<Route path="/about" element={<About />} />
 			<Route path="/kontakt" element={<Kontakt />} />
-			{/* <Route path="/cart" element={<Cart />} /> */}
+			<Route path="/cart" element={<Cart />} />
 			<Route path="/login" element={<Login />} />
 			<Route path="/*" element={<Notfound />} />
 		</Routes>
---
+++ src/Pages/Cart/Cart.jsx
@@ -0,0 +1,9 @@
+import { Layout } from "../../Components/App/Layout/Layout";
+
+export const Cart = () => {
+	return (
+		<>
+			<Layout title="Kurv"></Layout>
+		</>
+	);
+};
